221d494c76d5ae51620bc6a6de768faa0cab0257
maziokey <okeyluvsu2004@yahoo.com>
2021-12-24 23:11:48 +0100

    debug off

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = True
 
 ALLOWED_HOSTS = ['abujapr.com', 'www.abujapr.com', 'stkevinsjikwoyi.abujapr.com']
